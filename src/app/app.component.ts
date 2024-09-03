7f0bb19920f2b76ee97682ae1a5ece3286955746
miguelmoreira7 <miguelmoreiraneto@hotmail.com>
2024-09-03 01:13:19 -0300

    login e registro finalizados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { FormsModule } from '@angular/forms';
+import { RegistroComponent } from './registro/registro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, FormsModule, RegistroComponent, HttpClientModule, RouterLink, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
